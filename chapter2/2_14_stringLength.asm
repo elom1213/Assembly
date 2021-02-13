cc92664d84bc652b836d772e0c775b62a14d820b
jihunPark <junny.park1213@gmail.com>
2021-02-14 01:02:56 +0900

    delete txt, hello file. modify 2_14

--- chapter2/2_14_stringLength.asm
+++ chapter2/2_14_stringLength.asm
@@ -3,15 +3,6 @@ test_string:
       db      "Hello Whatup", 0
       section .text
       global  _start
-      
-print: 
-      mov     rax, 1
-      mov     rdi, 1
-      mov     rsi, output
-      mov     rdx, 4
-      syscall
-      ret
-
 strlen:
       xor     rax, rax
 .loop:
@@ -26,16 +17,6 @@ strlen:
 _start:
       mov     rdi, test_string
       call    strlen
-
-      mov     dword[output], eax
-      push    rax
-      call    print
-
-      pop     rax
       mov     rdi, rax
       mov     rax, 60
       syscall
-
-      section .bss
-output:
-      resb    4
